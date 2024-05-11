4314de2aeafc25099599071455a51a9c127d94c4
ichirou2910 <ichiroukeita2910@gmail.com>
2024-05-11 16:50:10 +0700

    feat: constants

---
+++ lib/utils/consts.dart
@@ -0,0 +1,9 @@
+class Consts {
+  static const String DB_TRANSACTION = "transaction";
+  static const String DB_TRANSACTION_GROUP = "transaction_group";
+  static const String DB_TRANSACTION_TRANSACTION_GROUP_MAPPING =
+      "transaction_transaction_group_mapping";
+  static const String DB_TRANSACTION_LABEL = "transaction_label";
+  static const String DB_TRANSACTION_TRANSACTION_LABEL_MAPPING =
+      "transaction_transaction_label_mapping";
+}
